498b6df1c971cb5fa2be026eaa96c0f104a9aa47
black064 <125512853+black064@users.noreply.github.com>
2024-05-14 21:34:03 -0400

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -13,11 +13,11 @@ module.exports = {
       .setColor('#2b71ec')
       .setAuthor({
         name: 'Im here to Help!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
+        iconURL: 'https://cdn.discordapp.com/attachments/851992268143722497/1240114780702769282/363817b96d946781.png?ex=66456263&is=664410e3&hm=748e7d290cd4666c174390c3b436a0626a44c90dbd19e3c24a2b94012b5c980e&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/@Black0644/videos)**\n\n__**COMMANDS :**__ `)
       .addFields(
       
         {
@@ -57,16 +57,16 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=6641e8b7&is=66409737&hm=96f69ba8bcde4ca44bf8713e22001e0bafad0335cfd1f57c439a34b4762f0e03&`);
+      .setImage(`https://cdn.discordapp.com/attachments/851992268143722497/1240114780702769282/363817b96d946781.png?ex=66456263&is=664410e3&hm=748e7d290cd4666c174390c3b436a0626a44c90dbd19e3c24a2b94012b5c980e&`);
 
     const button1 = new ButtonBuilder()
       .setLabel('YouTube')
-      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setURL('https://www.youtube.com/@Black0644/videos')
       .setStyle(ButtonStyle.Link);
 
     const button2 = new ButtonBuilder()
       .setLabel('Discord')
-      .setURL('https://discord.gg/xQF9f9yUEM')
+      .setURL('https://discord.gg/DwpBEJZV99')
       .setStyle(ButtonStyle.Link);
 
     const button3 = new ButtonBuilder()
